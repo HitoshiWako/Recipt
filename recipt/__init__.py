06068a3955fcb56e166c9157b972582a29cd9bb7
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2021-12-30 20:51:42 +0900

    configファイルを使用

--- recipt/__init__.py
+++ recipt/__init__.py
@@ -4,8 +4,7 @@ from recipt.database import init_db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object('recipt.config.Config')
 
     init_db(app)
 
