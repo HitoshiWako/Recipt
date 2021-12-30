2befc7903a985db1ae77d1098760b12ffe0ec8aa
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2021-12-30 23:06:31 +0900

    割引額を追加

--- recipt/model.py
+++ recipt/model.py
@@ -7,6 +7,7 @@ class Item(db.Model):
     date = db.Column(db.Date,default=datetime.now())
     name = db.Column(db.String(128))
     price = db.Column(db.Integer)
+    discount = db.Column(db.Integer,default=0)
 
     def __repr__(self) -> str:
-        return f'<Item id:{self.id} date:{self.date} name:{self.name} price:{self.price}>'
+        return f'<Item id:{self.id} date:{self.date} name:{self.name} price:{self.price} discount:{self.discount}>'
