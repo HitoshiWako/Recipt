5c90c89c06ee398e9c393013ac13b64666cb26e3
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2021-12-30 22:53:09 +0900

    date Columnを追加

--- recipt/__init__.py
+++ recipt/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from recipt.database import init_db
+from recipt.model import Item
 
 def create_app():
     app = Flask(__name__)
--- recipt/model.py
+++ recipt/model.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from recipt.database import db
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date,default=datetime.now())
     name = db.Column(db.String(128))
     price = db.Column(db.Integer)
 
     def __repr__(self) -> str:
-        return f'<Item id:{self.id} name:{self.name} price:{self.price}>'
+        return f'<Item id:{self.id} date:{self.date} name:{self.name} price:{self.price}>'
