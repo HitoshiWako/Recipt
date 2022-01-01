7151ec5ff47ee7c299760dbc6da5226c5cdc2ae5
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2022-01-01 11:51:44 +0900

    Shopページ完了

--- recipt/shop.py
+++ recipt/shop.py
@@ -1,9 +1,16 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request
+
+from .database import db
 from .model import Shop
 
 bp = Blueprint('shop',__name__)
 
 @bp.route('/shop',methods=('GET','POST'))
 def register():
+    if request.method == 'POST':
+        name = request.form['shop_name']
+        shop = Shop(name = name)
+        db.session.add(shop)
+        db.session.commit()
     shops = Shop.query.all()
     return render_template('shop.html',shops=shops)
